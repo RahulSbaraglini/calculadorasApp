6f09f88722f05f08d80f988aa74126195d099934
RahulSbaraglini <60709828+RahulSbaraglini@users.noreply.github.com>
2020-07-08 12:20:13 -0300

    Update tab2.page.ts

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -13,18 +13,21 @@ export class Tab2Page {
   public resultado: string; //null
 
   private ponto = false;
-
+  
+  //variável para as contas
   private operacoes = ['+', '-', '*', '/'];
 
   constructor(public alertController: AlertController) { }
 
+  //apagar para próxima conta
   public adicionarNumero(valor: string) {
     if(this.resultado){
       this.apagarTudo();
     }
     this.calculo = this.calculo + valor;
   }
-
+  
+  //adicionar o ponto na conta e fazer com que ele não se repita no mesmo número
   public adicionarPonto() {
     if (this.ponto) {
       return;
@@ -33,7 +36,8 @@ export class Tab2Page {
     this.calculo += ".";
     this.ponto = true;
   }
-
+  
+  //fazer com que o ponto se repita após as contas
   public adicionarOperacao(operador: string) {
 
     if(this.resultado){
@@ -48,13 +52,15 @@ export class Tab2Page {
     this.calculo += operador;
     this.ponto = false;
   }
-
+  
+  //Configuração do botão C da calculadora, que o objetivo é: apagar tudo
   public apagarTudo() {
     this.calculo = '';
     this.resultado = null;
     this.ponto = false;
   }
 
+  //Configuração do botão DEL, que o objetivo é: apagar o último número
   public apagarUltimo() {
     const ultimo = this.calculo.slice(-1);
     if (ultimo == '.') {
@@ -63,7 +69,8 @@ export class Tab2Page {
 
     this.calculo = this.calculo.slice(0, -1);
   }
-
+  
+  //calcular o resultado das contas
   public calcularResultado() {
     try {
       this.resultado = evaluate(this.calculo);
@@ -72,7 +79,8 @@ export class Tab2Page {
       this.presentAlert('ERRO!!!', 'Cálculo inválido, verifique!');
     }
   }
-
+  
+  //configuração da mensagem de erro
   async presentAlert(titulo: string, mensagem: string) {
     const alert = await this.alertController.create({
       header: 'ERRO!!!',
